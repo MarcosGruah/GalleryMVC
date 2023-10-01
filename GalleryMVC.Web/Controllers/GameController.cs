918dece0ffe384089adea6a03f7e3eb389fc6ea8
MarcosGruah <110102063+MarcosGruah@users.noreply.github.com>
2023-10-01 10:50:31 -0300

    Games table content visible on the website

--- GalleryMVC.Web/Controllers/GameController.cs
+++ GalleryMVC.Web/Controllers/GameController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GalleryMVC.Web.Data;
+using GalleryMVC.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GalleryMVC.Web.Controllers
 {
     public class GameController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public GameController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Game> objGameList = _db.Games.OrderBy(game => game.Name).ToList();
+            return View(objGameList);
         }
     }
 }
