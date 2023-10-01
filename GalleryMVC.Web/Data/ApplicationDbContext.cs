2d5e0060003ea943ef1094467ab71e776254f09a
MarcosGruah <110102063+MarcosGruah@users.noreply.github.com>
2023-10-01 09:37:26 -0300

    SQL Database Connection

---
+++ GalleryMVC.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GalleryMVC.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
